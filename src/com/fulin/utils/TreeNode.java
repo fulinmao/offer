6cd58dba3d710dd4f9e69c18a2d1b9f4e110617b
‘“Maop FuLin <840060109@qq.com>
2019-07-06 15:20:52 +0800

    1.新增TreeNode类

---
+++ src/com/fulin/utils/TreeNode.java
@@ -0,0 +1,16 @@
+package com.fulin.utils;
+
+public class TreeNode {
+
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+
+
+    public TreeNode(int val){
+        this.val = val;
+        this.left = null;
+        this.right = null;
+    }
+
+}
